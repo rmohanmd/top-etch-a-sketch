7f80be9e83733357bc0b27ca5b9d051c68c1a86d
rmohanmd <rmohan.md@gmail.com>
2022-08-23 17:19:11 -0400

    Added css style to center and square etch-a-sketch pixels
    Created a wrapper div to contain the entire project.
    Used css stylesheet to create a square for the container div.
    
    Javascript added to calculate the size of padding for each pixel and inset inline
    style element dictating the padding. Gives a perfect square of identical shaped pixels.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,17 @@
 const container = document.querySelector(".container");
 
-let panels = 0;
-panels = 10;
-for (i = 0; i < panels; i++) {
-  const pixels = document.createElement("div");
-  pixels.classList.add("pixel");
-  pixels.textContent = "hello";
-  container.appendChild(pixels);
+let dimension = 25;
+
+const pads = 720 / 2 / dimension;
+for (y = 0; y < dimension; y++) {
+  for (i = 0; i < dimension; i++) {
+    const pixels = document.createElement("div");
+    pixels.classList.add("pixel");
+    container.appendChild(pixels);
+    pixels.style.padding = `${pads}px`;
+  }
+
+  //   const lineBreak = document.createElement("div");
+  //   lineBreak.classList.add("break");
+  //   container.appendChild(lineBreak);
 }
