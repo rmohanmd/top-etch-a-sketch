d08af758844d13735c780cb0aca97e78acef45a2
rmohanmd <rmohan.md@gmail.com>
2022-08-23 18:53:39 -0400

    Added button to change canvas size and canvas size label
    Reorganized functions in js file to allow redrawing of the canvas.
    
    Added button to allow user to change canvas size to max size of 100.
    
    Used css to style and align button and canvas size indicator.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,46 @@
 const container = document.querySelector(".container");
 
-let dimension = 100;
+let dimension = 10;
 
-const pads = 720 / 2 / dimension;
-for (y = 0; y < dimension; y++) {
-  for (i = 0; i < dimension; i++) {
-    const pixels = document.createElement("div");
-    pixels.classList.add("pixel");
-    container.appendChild(pixels);
-    pixels.style.padding = `${pads}px`;
+function generatePad() {
+  container.textContent = "";
+  const pads = 720 / 2 / dimension;
+  const sizeLabel = document.querySelector(".size");
+  sizeLabel.textContent = `Canvas Size:${dimension}`;
+  for (y = 0; y < dimension; y++) {
+    for (i = 0; i < dimension; i++) {
+      const pixels = document.createElement("div");
+      pixels.classList.add("pixel");
+      container.appendChild(pixels);
+      pixels.style.padding = `${pads}px`;
+    }
   }
+  return;
 }
 
-const pixel = document.querySelectorAll(".pixel");
-pixel.forEach((pixel) =>
-  pixel.addEventListener("mouseenter", (e) => pixel.classList.add("highlight"))
-);
+function draw() {
+  const pixel = document.querySelectorAll(".pixel");
+  pixel.forEach((pixel) =>
+    pixel.addEventListener("mouseenter", (e) =>
+      pixel.classList.add("highlight")
+    )
+  );
+}
+
+function button() {
+  const button = document.querySelector(".button");
+  button.addEventListener("click", function () {
+    dimension = prompt(
+      "To change your sketchpad size,enter a number between 1 - 100"
+    );
+    if (dimension > 100) dimension = 100;
+    else if (dimension < 1) dimension = 1;
+
+    generatePad();
+    draw();
+  });
+}
+
+generatePad();
+draw();
+button();
